2526075ef1057f8ae67013d9f4f41d5d65a804aa
55 <tingrungchang@gmail.com>
2020-04-24 20:25:35 +0800

    新

--- music.js
+++ music.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var track = document.getElementById('track');
 
 var controlBtn = document.getElementById('play-pause');
@@ -18,3 +19,25 @@ controlBtn.addEventListener("click", playPause);
 track.addEventListener("ended", function() {
   controlBtn.className = "play";
 });
+=======
+var track = document.getElementById('track');
+
+var controlBtn = document.getElementById('play-pause');
+
+function playPause() {
+    if (track.paused) {
+        track.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn.className = "pause";
+    } else {
+        track.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn.className = "play";
+    }
+}
+
+controlBtn.addEventListener("click", playPause);
+track.addEventListener("ended", function() {
+  controlBtn.className = "play";
+});
+>>>>>>> 3c0ce1e71dad063e2da087c0cf7c934d19f69468
